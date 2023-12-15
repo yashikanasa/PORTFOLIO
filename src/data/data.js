f65913f83de22d60e85881225dc85c661abea895
Y A S H I K A <117389788+yashikanasa@users.noreply.github.com>
2023-12-15 18:42:06 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/XXMcBwy1/image-1.png',
+    title: "Invoice Flow",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/1tcWQ7nR/image.png',
+    title: "Hostel Vibe",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/tT8pPTRm/image-2.png',
+    title: "ResideHub",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
